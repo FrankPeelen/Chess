3ceb24ed2cd188c5f6a31462b5fb8f28e53672d5
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-04-24 14:42:26 +0900

    Complete Game's initializer

--- lib/controllers/game.rb
+++ lib/controllers/game.rb
@@ -3,5 +3,16 @@ require_relative "../models/board"
 
 class Game
 	def initialize
+		@player1 = new_player(1)
+		@player2 = new_player(2)
+		@board = Board.new(@player1, @player2)
+	end
+
+	private
+	def new_player(num)
+		puts "Please enter player#{num}'s name:"
+		player = Player.new(gets.chomp)
+		puts "Welcome to the game #{player.name}!"
+		player
 	end
 end
--- spec/controllers/game_spec.rb
+++ spec/controllers/game_spec.rb
@@ -1,10 +1,17 @@
 require "./spec_helper"
 require "./../lib/controllers/game"
+require "./../lib/models/player"
 
 describe Game do
+	let(:player1_name) { "Frank" }
+	let(:player2_name) { "Kasparov" }
+	before { Game.any_instance.stub(:puts)
+					 Game.any_instance.stub(:gets).and_return(player1_name, player2_name) }
 	subject(:game) { Game.new }
 
 	describe "#new" do
-		#it { expect(game.instance_variable_get(:@board)).not_to be_nil }
+		it { expect(game.instance_variable_get(:@board)).not_to be_nil }
+		it { expect(game.instance_variable_get(:@player1).name).to eql(player1_name) }
+		it { expect(game.instance_variable_get(:@player2).name).to eql(player2_name) }
 	end
 end
