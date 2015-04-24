74ebdfdb94210738e6bfb2f717d83012da184987
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-04-24 14:09:43 +0900

    Made Board initialize with Pieces

--- lib/models/board.rb
+++ lib/models/board.rb
@@ -1,10 +1,45 @@
+require_relative "pieces"
+
 class Board
 	attr_reader :field
 
 	def initialize(player1, player2)
+		@field = construct_empty_field(player1, player2)
+	end
+
+	private
+	def construct_empty_field(player1, player2)
+		empty_field = []
+		empty_field.push(construct_row_1_8(player1))
+		empty_field.push(construct_row_2_7(player1))
+		4.times { empty_field.push(construct_empty_row) }
+		empty_field.push(construct_row_2_7(player2))
+		empty_field.push(construct_row_1_8(player2))
+		empty_field
+	end
+
+	def construct_empty_row
 		empty_row = []
 		8.times { empty_row.push(nil) }
-		@field = []
-		8.times { @field.push(empty_row) }
+		empty_row
+	end
+
+	def construct_row_1_8(player)
+		row = []
+		row.push(Rook.new(player))
+		row.push(Knight.new(player))
+		row.push(Bishop.new(player))
+		row.push(Queen.new(player))
+		row.push(King.new(player))
+		row.push(Bishop.new(player))
+		row.push(Knight.new(player))
+		row.push(Rook.new(player))
+		row
+	end
+
+	def construct_row_2_7(player)
+		row = []
+		8.times { row.push(Pawn.new(player)) }
+		row
 	end
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,6 +1,7 @@
 require "../spec_helper"
 require "../../lib/models/board"
 require "../../lib/models/player"
+require "../../lib/models/pieces"
 
 describe Board do
 	let(:name1) { "Frank" }
@@ -10,19 +11,40 @@ describe Board do
 	subject(:board) { Board.new(player1, player2) }
 
 	describe "#new" do
-		let(:empty_row) {
-			row = []
-			8.times { row.push(nil) }
-			row }
-		let(:field) { [empty_row,
-									 empty_row,
-									 empty_row,
-									 empty_row,
-									 empty_row,
-									 empty_row,
-									 empty_row,
-									 empty_row] }
+		let(:empty_rows_correct) {
+			(3..6).each do |row_num|
+				board.field[row_num - 1].each do |val|
+					return false unless val == nil
+				end
+			end
+			return true }
+		let(:pawn_rows_correct) {
+			board.field[1].each do |val|
+				return false unless val.is_a?(Pawn) && val.owner == player1
+			end
+			board.field[6].each do |val|
+				return false unless val.is_a?(Pawn) && val.owner == player2
+			end
+			return true }
+		let(:back_rows_correct) {
+			row1 = board.field[0]
+			row8 = board.field[7]
+			row1.each do |val|
+				return false unless val.owner == player1
+			end
+			row8.each do |val|
+				return false unless val.owner == player2
+			end
+			return false unless row1[0].is_a?(Rook) && row1[7].is_a?(Rook) && row8[0].is_a?(Rook) && row8[7].is_a?(Rook)
+			return false unless row1[1].is_a?(Knight) && row1[6].is_a?(Knight) && row8[1].is_a?(Knight) && row8[6].is_a?(Knight)
+			return false unless row1[2].is_a?(Bishop) && row1[5].is_a?(Bishop) && row8[2].is_a?(Bishop) && row8[5].is_a?(Bishop)
+			return false unless row1[3].is_a?(Queen) && row8[3].is_a?(Queen)
+			return false unless row1[4].is_a?(King) && row8[4].is_a?(King)
+			return true }
 
-		it { expect(board.field).to eql(field) }
+		it { expect(board.field.size).to eql(8) }
+		it { expect(empty_rows_correct).to be_true }
+		it { expect(pawn_rows_correct).to be_true }
+		it { expect(back_rows_correct).to be_true }
 	end
 end
