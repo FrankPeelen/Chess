e09a0ea488e510f4449e5e061404f5dc9c001b62
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-04-24 14:25:15 +0900

    Added empty controller Game

---
+++ lib/controllers/game.rb
@@ -0,0 +1,7 @@
+require_relative "../models/player"
+require_relative "../models/board"
+
+class Game
+	def initialize
+	end
+end
---
+++ spec/controllers/game_spec.rb
@@ -0,0 +1,10 @@
+require "./spec_helper"
+require "./../lib/controllers/game"
+
+describe Game do
+	subject(:game) { Game.new }
+
+	describe "#new" do
+		#it { expect(game.instance_variable_get(:@board)).not_to be_nil }
+	end
+end
